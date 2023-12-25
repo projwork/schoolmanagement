d40d48793a287fc51c7d5e9c10645d6a0b6832c0
Biruk Anose <biruk4work@gmail.com>
2023-12-25 13:49:52 +0300

    add starting controllers with minimum logic

---
+++ routes/staff/adminRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const {
+  registerAdminCtrl,
+  loginAdminCtrl,
+  getAdminsCtrl,
+  updateAdminCtrl,
+  deleteAdminCtrl,
+  adminSuspendTeacherCtrl,
+  adminUnSuspendTeacherCtrl,
+  adminWithdrawTeacherCtrl,
+  adminUnWithdrawCtrl,
+  adminPublishResultsCtrl,
+  adminUnPublishResultsCtrl,
+} = require("../../controller/staff/adminCtrl");
+
+const adminRouter = express.Router();
+
+// register
+adminRouter.post("/register", registerAdminCtrl);
+
+// login
+adminRouter.post("/login", loginAdminCtrl);
+
+// get all admin
+adminRouter.get("/", getAdminsCtrl);
+
+// get single admin
+adminRouter.get("/:id", getAdminsCtrl);
+
+// update admin
+adminRouter.put("/:id", updateAdminCtrl);
+
+// delete admin
+adminRouter.delete("/:id", deleteAdminCtrl);
+
+// admin suspending a teacher
+adminRouter.put("/suspend/teacher/:id", adminSuspendTeacherCtrl);
+
+// admin Unsuspending a teacher
+adminRouter.put("/unsuspend/teacher/:id", adminUnSuspendTeacherCtrl);
+
+// admin withdrawing a teacher
+adminRouter.put("/withdraw/teacher/:id", adminWithdrawTeacherCtrl);
+
+// admin unwithdrawing a teacher
+adminRouter.put("/unwithdraw/teacher/:id", adminUnWithdrawCtrl);
+
+// admin publish exam results
+adminRouter.put("/publish/exam/:id", adminPublishResultsCtrl);
+
+// admin unpublish exam results
+adminRouter.put("/unpublish/exam/:id", adminUnPublishResultsCtrl);
+
+module.exports = adminRouter;
