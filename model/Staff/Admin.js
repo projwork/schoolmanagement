9ea7773b5e7d636c3a760c56d021e6b0518dc3a7
Biruk Anose <biruk4work@gmail.com>
2023-12-27 18:44:08 +0300

    year group crud finished

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -30,6 +30,12 @@ const adminSchema = new mongoose.Schema(
         ref: "Program",
       },
     ],
+    yearGroups: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "YearGroup",
+      },
+    ],
     academicYears: [
       {
         type: mongoose.Schema.Types.ObjectId,
