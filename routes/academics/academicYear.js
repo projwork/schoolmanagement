bb8349f50f9a4f4281501f7818b3ba587b1354a3
Biruk Anose <biruk4work@gmail.com>
2023-12-27 11:56:55 +0300

    use express router chaining

--- routes/academics/academicYear.js
+++ routes/academics/academicYear.js
@@ -11,10 +11,22 @@ const isAdmin = require("../../middlewares/isAdmin");
 
 const academicYearRouter = express.Router();
 
-academicYearRouter.post("/", isLogin, isAdmin, createAcademicYear);
-academicYearRouter.get("/", isLogin, isAdmin, getAcademicYears);
-academicYearRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
-academicYearRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
-academicYearRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+// academicYearRouter.post("/", isLogin, isAdmin, createAcademicYear);
+// academicYearRouter.get("/", isLogin, isAdmin, getAcademicYears);
+
+academicYearRouter
+  .route("/")
+  .post(isLogin, isAdmin, createAcademicYear)
+  .get(isLogin, isAdmin, getAcademicYears);
+
+academicYearRouter
+  .route("/:id")
+  .get(isLogin, isAdmin, getAcademicYear)
+  .put(isLogin, isAdmin, updateAcademicYear)
+  .delete(isLogin, isAdmin, deleteAcademicYear);
+
+// academicYearRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
+// academicYearRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
+// academicYearRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
 
 module.exports = academicYearRouter;
